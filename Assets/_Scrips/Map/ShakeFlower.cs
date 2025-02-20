c13eadb96a11fb7a9bcd3afb86ae0dc8f1a4b10f
namdev26 <vuhoangnam310104@gmail.com>
2025-02-20 23:03:52 +0700

    tulip effect

---
+++ Assets/_Scrips/Map/ShakeFlower.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class ShakeFlower : MonoBehaviour
+{
+    private Quaternion originQuaternion;
+    [SerializeField] private float shakeSpeed = 0.1f;
+    [SerializeField] private float shakeTime = 1f;
+    [SerializeField] private float shakeAngle = 5f;
+    [SerializeField] private bool isShaking = false;
+
+    private void Start()
+    {
+        originQuaternion = transform.localRotation;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            StartCoroutine(ShakeEffect());
+        }
+    }
+
+    private IEnumerator ShakeEffect()
+    {
+        isShaking = true;
+        float time = 0f;
+
+        while (time < shakeTime)
+        {
+            time += Time.deltaTime;
+            // Sử dụng Mathf.PingPong để tạo dao động nghiêng rõ hơn
+            float angle = Mathf.Sin(time * shakeSpeed * Mathf.PI * 2) * shakeAngle;
+            transform.localRotation = originQuaternion * Quaternion.Euler(0, 0, angle);
+            yield return null;
+        }
+        transform.localRotation = originQuaternion;
+        isShaking = false;
+    }
+
+
+}
